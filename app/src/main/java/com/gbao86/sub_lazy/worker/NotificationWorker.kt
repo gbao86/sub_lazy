1f7fe6fe8e853e311e866962cbe8d8776dbfc22a
client100102 <Duybao23032003@gmail.com>
2026-06-02 12:59:34 +0700

    copyright: apply proprietary and confidential license header to all Kotlin source files

--- app/src/main/java/com/gbao86/sub_lazy/worker/NotificationWorker.kt
+++ app/src/main/java/com/gbao86/sub_lazy/worker/NotificationWorker.kt
@@ -1,3 +1,14 @@
+/*
+Copyright (C) 2026 Trịnh Gia Bảo (gbao86) <tiktokthu10@gmail.com>. All Rights Reserved.
+
+This file is part of Sub Lazy - A premium, modern subscription tracker and manager for Android.
+
+Unauthorized copying of this file, via any medium, is strictly prohibited.
+Proprietary and confidential.
+This source code is provided for reference purposes only and may not be copied, 
+modified, or distributed without the express written permission of the author.
+*/
+
 package com.gbao86.sub_lazy.worker
 
 import android.app.NotificationChannel
