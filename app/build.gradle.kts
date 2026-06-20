121980868dc6187d3075f8210d7de4c58318a305
gbao86 <Duybao23032003@gmail.com>
2026-06-20 18:30:04 +0700

    fix: downgrade Java Toolchain to 17 to fix CodeQL autobuild

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,11 +32,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_21
-        targetCompatibility = JavaVersion.VERSION_21
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlin {
-        jvmToolchain(21)
+        jvmToolchain(17)
     }
     buildFeatures {
         compose = true
